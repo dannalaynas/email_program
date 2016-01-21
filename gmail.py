005729ff93ca90016efcd1af55c19532e3fba6fa
dannalaynas <dannalaynas@gmail.com>
2016-01-21 09:21:04 -0800

    email successful

--- gmail.py
+++ gmail.py
@@ -2,9 +2,17 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 
 browser.get('http://gmail.com')
+
 emailElem = browser.find_element_by_id('Email')
-emailElem.send_keys('')
+emailElem.send_keys('yuba.danna@gmail.com')
+
+loginElem = browser.find_element_by_id('next')
+loginElem.click()
 
 passwordElem = browser.find_element_by_id('Passwd')
-passwordElem.send_keys('')
+passwordElem.send_keys('pie1234567')
+
 passwordElem.submit()
+
+loginElem = browser.find_element_by_id('signIn')
+loginElem.click()
