58bfdb3ea033a009df7aee9598efa5f17f60ec61
MiqueasLH <miqueaslaynas@gmail.com>
2016-01-21 23:30:35 -0800

    added subject, body, and recipiens

--- gmail.py
+++ gmail.py
@@ -16,3 +16,24 @@ passwordElem.submit()
 
 loginElem = browser.find_element_by_id('signIn')
 loginElem.click()
+
+composeElem = browser.find_element_by_class('T-I J-J5-Ji T-I-Ke L3')
+composeElem.click()
+
+print('Recipiens\' Email?')
+sendTo = input()
+
+sendtoElem = browser.find_element_by_id(':9w')
+sendtoElem.send_keys(sendTo)
+
+print('Subject')
+subjectline = input()
+
+subjectElem = browser.find_element_by_id(':9h')
+subjectElem.send_keys(subjectLine)
+
+print('Input Your Messege')
+bodyline = input()
+
+sendtoElem = browser.find_element_by_id(':97')
+sendtoElem.send_keys (bodyLine)
